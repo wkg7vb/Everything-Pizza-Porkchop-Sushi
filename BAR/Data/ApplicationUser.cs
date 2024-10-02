6196d95c297a0e34d00a3925d96c4e7d9ffd1f92
wkg7vb <90648776+wkg7vb@users.noreply.github.com>
2024-10-02 16:41:45 -0500

    Initialized Database Design

--- BAR/Data/ApplicationUser.cs
+++ BAR/Data/ApplicationUser.cs
@@ -1,10 +1,38 @@
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace BAR.Data
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
-    }
+        [Key]
+        //public required int Id { get; set; }
 
+        //auth
+        //public required string Email { get; set; }
+        public required string Username { get; set; }
+        public required string Password { get; set; } //STORE ONLY A HASH OF THIS!
+
+        //budget
+        public decimal Income { get; set; }
+        public decimal Housing { get; set; }
+        public decimal BillsUtilities { get; set; }
+        public decimal GroceryDining { get; set; }
+        public decimal Transport { get; set; }
+        public decimal Education { get; set; }
+        public decimal Debt { get; set; }
+        public decimal Entertainment { get; set; }
+        public decimal Shopping { get; set; }
+        public decimal Medical { get; set; }
+        public decimal Investing { get; set; }
+        public decimal Misc { get; set; }
+
+        //profile
+        public required string FName { get; set; }
+        public required string LName { get; set; }
+        //settings
+        public bool Darkmode { get; set; }
+        public char Currency { get; set; }
+    }
 }
---
+++ BAR/Data/Transactions.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BAR.Data
+{
+    public class Transactions
+    {
+        [Key]
+        public required int Id { get; set; }
+        public required string Category { get; set; }
+        public required string Amount { get; set; }
+        public required string Date { get; set; }
+    }
+}
