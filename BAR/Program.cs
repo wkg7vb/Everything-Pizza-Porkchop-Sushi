525c57e11e74da1e90af80465099b24d9fe6740e
egurung11 <elainagurung@gmail.com>
2024-10-03 13:49:17 -0500

    added blazor bootstrap
    added blazor bootstrap to branch so it would work :)

--- BAR/Program.cs
+++ BAR/Program.cs
@@ -11,6 +11,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddBlazorBootstrap();
+
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddScoped<IdentityUserAccessor>();
 builder.Services.AddScoped<IdentityRedirectManager>();
