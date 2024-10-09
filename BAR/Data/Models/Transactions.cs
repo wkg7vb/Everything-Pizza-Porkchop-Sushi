bf71a471a5c9b4c68f2e92bb419d1d1aa5a19097
wkg7vb <90648776+wkg7vb@users.noreply.github.com>
2024-10-09 15:24:55 -0500

    commit 1

--- BAR/Data/Models/Transactions.cs
+++ BAR/Data/Models/Transactions.cs
@@ -4,13 +4,26 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BAR.Data.Models
 {
-    public class Transaction
+    public class Transaction : ApplicationUser
     {
         [Key]
         public required string TimeStamp { get; set; }
         [ForeignKey("Id")]
         public required string Uid { get; set; }
+        //user text input
         public required string Label { get; set; }
+       
+        [AllowedValues("Housing",
+            "Bills/Utilities",
+            "Grocery/Dining",
+            "Transportation",
+            "Education",
+            "Debt",
+            "Entertainment",
+            "Shopping",
+            "Medical",
+            "Investing",
+            "Miscellaneous")]
         public required string Category { get; set; }
         public required decimal Amount { get; set; }
     }
