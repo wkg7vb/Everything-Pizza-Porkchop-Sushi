519f58204bccc572670cd114139016ce3db4669b
wkg7vb <90648776+wkg7vb@users.noreply.github.com>
2024-10-09 10:34:08 -0500

    CRUD for transaction history
    UI not started. I will need some assistance. thx

--- BAR/Data/Models/Transactions.cs
+++ BAR/Data/Models/Transactions.cs
@@ -4,14 +4,14 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BAR.Data.Models
 {
-    public class Transaction : IdentityUser
+    public class Transaction
     {
         [Key]
-        public required byte[] TimeStamp { get; set; }
+        public required string TimeStamp { get; set; }
         [ForeignKey("Id")]
         public required string Uid { get; set; }
         public required string Label { get; set; }
         public required string Category { get; set; }
-        public required string Amount { get; set; }
+        public required decimal Amount { get; set; }
     }
 }
