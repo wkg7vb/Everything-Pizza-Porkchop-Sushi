5d31c11b370a59f4ab433015f022044b66acc1a8
max-dejesus <90429251+max-dejesus@users.noreply.github.com>
2024-11-22 15:14:41 -0600

    Revert "Wg add and update modal (#47)" (#50)
    This reverts commit c9eb447f0522c0513f47ac4abccb71d6c6539c94.

--- BAR/Data/Models/UserTransaction.cs
+++ BAR/Data/Models/UserTransaction.cs
@@ -6,7 +6,6 @@ public class UserTransaction
     [Key]
     public int TransactionId { get; set; }
     public DateTime TransactionDateTime { get; set; }
-    //public DateOnly TransactionDateOnly { get; set; }
     public string TransactionLabel { get; set; }
     public decimal TransactionAmt { get; set; }
     public string TransactionCategory { get; set; }
