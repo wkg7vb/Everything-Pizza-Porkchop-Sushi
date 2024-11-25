0dad9b146a9051b192412fce99155418c5fa33fb
egurung11 <elainagurung@gmail.com>
2024-11-25 12:05:32 -0600

    transactionlist

---
+++ BAR/Data/Migrations/20241125173818_jib.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BAR.Migrations
+{
+    /// <inheritdoc />
+    public partial class jib : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TransasctionCategory",
+                table: "UserTransactions",
+                newName: "TransactionCategory");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TransactionCategory",
+                table: "UserTransactions",
+                newName: "TransasctionCategory");
+        }
+    }
+}
